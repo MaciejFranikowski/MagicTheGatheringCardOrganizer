7a702c869e4ab1cfd287dba321cfefd2790c7f59
Maciej Franikowski <mfranikowski@gmail.com>
2024-02-28 19:10:34 +0100

    Create Spring Boot project

---
+++ src/test/java/com/maciejfranikowski/magicTheGatheringCardOrganizer/MagicTheGatheringCardOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciejfranikowski.magicTheGatheringCardOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTheGatheringCardOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
