7f111c6614e52caffec56abbf5bbb00f05924fca
Maciej Franikowski <mfranikowski@gmail.com>
2024-03-11 20:41:56 +0100

    Add findByName method for Card Boxes

--- src/main/java/com/maciejfranikowski/magicTheGatheringCardOrganizer/repository/CardBoxDao.java
+++ src/main/java/com/maciejfranikowski/magicTheGatheringCardOrganizer/repository/CardBoxDao.java
@@ -4,7 +4,10 @@ import com.maciejfranikowski.magicTheGatheringCardOrganizer.models.CardBox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface CardBoxDao extends JpaRepository<CardBox, Integer> {
+    public Optional<CardBox> findByName(String name);
 }
