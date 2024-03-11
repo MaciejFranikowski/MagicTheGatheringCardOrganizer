9ed316fb3042596f4645a855321d1a60d91fdd5a
Maciej Franikowski <mfranikowski@gmail.com>
2024-03-11 18:57:58 +0100

    Add getting all card boxes functionality to the box and card service.

--- src/main/java/com/maciejfranikowski/magicTheGatheringCardOrganizer/service/BoxAndCardService.java
+++ src/main/java/com/maciejfranikowski/magicTheGatheringCardOrganizer/service/BoxAndCardService.java
@@ -1,7 +1,18 @@
 package com.maciejfranikowski.magicTheGatheringCardOrganizer.service;
 
+import com.maciejfranikowski.magicTheGatheringCardOrganizer.models.CardBox;
+import com.maciejfranikowski.magicTheGatheringCardOrganizer.repository.CardBoxDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class BoxAndCardService {
+    private final CardBoxDao cardBoxDao;
+    public BoxAndCardService(@Autowired CardBoxDao cardBoxDao){
+        this.cardBoxDao =  cardBoxDao;
+    }
+
+    public Iterable<CardBox> getCardBoxes(){return cardBoxDao.findAll();}
 }
