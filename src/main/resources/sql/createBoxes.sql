3f41216ff6103da04b3516a419fb4449f9295439
Maciej Franikowski <mfranikowski@gmail.com>
2024-03-11 20:39:43 +0100

    Update Sql for better readability

--- src/main/resources/sql/createBoxes.sql
+++ src/main/resources/sql/createBoxes.sql
@@ -1,4 +1,4 @@
-INSERT INTO `box` VALUES
+INSERT INTO `box` (id, name, location, color) VALUES
       (2,'second box', 'in the cupboard', 'black'),
       (3,'third box', 'in the drawers', 'red'),
       (4,'fourth box', 'under the tv', 'blue')
