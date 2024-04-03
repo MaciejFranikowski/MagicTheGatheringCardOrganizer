83f04da38ec49c6022bd79b1aba4867229cd3ff3
Maciej Franikowski <mfranikowski@gmail.com>
2024-04-03 22:36:15 +0200

    Add exception handling for the RestTemplate API call. Create and inject the RestTemplate for the whole app.

--- src/main/java/com/maciejfranikowski/magicTheGatheringCardOrganizer/service/ScryFallApiService.java
+++ src/main/java/com/maciejfranikowski/magicTheGatheringCardOrganizer/service/ScryFallApiService.java
@@ -1,9 +1,14 @@
 package com.maciejfranikowski.magicTheGatheringCardOrganizer.service;
 
-import com.maciejfranikowski.magicTheGatheringCardOrganizer.api.AutocompleteCards;
+import com.maciejfranikowski.magicTheGatheringCardOrganizer.api.AutoCompleteCards;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -11,15 +16,27 @@ import java.util.logging.Logger;
 public class ScryFallApiService {
     private final String SCRYFALL_API_AUTOCOMPLETE_URL="https://api.scryfall.com/cards/autocomplete?q=";
     private final Logger logger;
-    public ScryFallApiService(){
+    private final RestTemplate restTemplate;
+    public ScryFallApiService(@Autowired RestTemplate restTemplate){
        this.logger = Logger.getLogger(getClass().getName());
+       this.restTemplate = restTemplate;
     }
 
-    public List<String> searchAutoCompleteCardNames(String query){
-        RestTemplate restTemplate = new RestTemplate();
-        AutocompleteCards cards = restTemplate.getForObject(SCRYFALL_API_AUTOCOMPLETE_URL+query, AutocompleteCards.class);
-        logger.info(cards.toString());
-        return cards.getData();
-//        return Arrays.asList(cards);
+    public List<String> searchAutoCompleteCardNames(String query) {
+        try {
+            AutoCompleteCards cards = this.restTemplate.getForObject(
+                    SCRYFALL_API_AUTOCOMPLETE_URL + query,
+                    AutoCompleteCards.class
+            );
+            assert cards != null;
+            return cards.getData();
+        } catch (HttpClientErrorException | HttpServerErrorException httpClientException) {
+            logger.warning("Error calling external API: " +
+                    httpClientException.getStatusCode() + " " + httpClientException.getStatusText());
+            return Collections.emptyList();
+        } catch (RestClientException restClientException) {
+            logger.severe("Error calling external API: " + restClientException.getMessage());
+            return Collections.emptyList();
+        }
     }
 }
